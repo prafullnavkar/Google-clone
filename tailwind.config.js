0f6ba9010c50a4e3663a5b79aed42ebfc02bbc87
prafullnavkar <87638003+prafullnavkar@users.noreply.github.com>
2022-06-26 18:11:25 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  // to overide material ui css or to change color, etc of material ui compnent
+  important: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
